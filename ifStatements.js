d3e5a9c595ec93a71a79208c6dae7589e7f558e9
Erin Kelley <erin.kelley01@gmail.com>
2023-11-11 15:30:23 -0700

    Add ifStatement answers

--- ifStatements.js
+++ ifStatements.js
@@ -8,11 +8,13 @@
 // Write a conditional statement that prints only one or the other.
 // Experiment with manipulating the value held within numQuarters to make sure both conditions can be achieved.
 
-var numQuarters = 0;
-
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
-
+//var numQuarters = 0;
+//if (numQuarters >= 2){
+    //console.log("I have enough money for a gumball");
+//}
+//if (numQuarters < 2){
+    //console.log("I don't have enough money for a gumball");
+//}
 
 // CHALLENGE 2
 // Using the variables defined below, write code that will tell you if you have the ingredients to make a pizza. 
@@ -26,6 +28,16 @@ console.log("I don't have enough money for a gumball");
 
 // Experiment with manipulating the value held within both variables to make sure all above conditions output what you expect.
 
-var cupsOfFlour = 1;
+var cupsOfFlour = 3;
 var hasSauce = true;
+if (cupsOfFlour === 1 && hasSauce === true){
+    console.log("I cannot make pizza")
+} else if (cupsOfFlour === 5 && hasSauce === false){
+    console.log("I cannot make pizza")
+} else if (cupsOfFlour === 2 && hasSauce === true){
+    console.log("I can make pizza")}
+        else if (cupsOfFlour === 3 && hasSauce === true){
+            console.log("I can make pizza")
+        }
+
 
